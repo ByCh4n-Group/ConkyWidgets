2aac4213304c82904f96be65aefa6c7c2156bb5c
Özgür <68022135+mhykoI@users.noreply.github.com>
2022-01-09 15:28:35 +0300

    Update conky-startup.sh

--- conky-startup.sh
+++ conky-startup.sh
@@ -1,12 +1,16 @@
 sleep 0s
 killall conky
-cd "/home/$USER/.conky/File"
-conky -c "/home/$USER/.conky/File/File" &
-cd "/home/$USER/.conky/Saat"
-conky -c "/home/$USER/.conky/Saat/Saat" &
-cd "/home/$USER/.conky/Sistem"
-conky -c "/home/$USER/.conky/Sistem/Sistem" &
-cd "/home/$USER/.conky/Spotify"
-conky -c "/home/$USER/.conky/Spotify/Spotify" &
-cd "/home/$USER/.conky/Status"
-conky -c "/home/$USER/.conky/Status/Status" &
+cd "/home/$USER/.conky/Clock-Widget"
+conky -c "/home/$USER/.conky/Clock-Widget/Clock" &
+
+cd "/home/$USER/.conky/File-Widget"
+conky -c "/home/$USER/.conky/File-Widget/File" &
+
+cd "/home/$USER/.conky/System-Widget"
+conky -c "/home/$USER/.conky/System-Widget/System" &
+
+cd "/home/$USER/.conky/Spotify-Widget"
+conky -c "/home/$USER/.conky/Spotify-Widget/Spotify" &
+
+cd "/home/$USER/.conky/Status-Widget"
+conky -c "/home/$USER/.conky/Status-Widget/Status" &
